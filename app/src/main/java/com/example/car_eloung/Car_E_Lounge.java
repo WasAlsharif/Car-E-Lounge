ac5a4f06ca42c0d9b6c282b4d307b82419f89b29
WasAlsharif <waseem.m.alsharif@gmail.com>
2021-01-27 22:36:19 +0300

    Finished Design of Main activity

--- app/src/main/java/com/example/car_eloung/Car_E_Lounge.java
+++ app/src/main/java/com/example/car_eloung/Car_E_Lounge.java
@@ -1,14 +1,17 @@
 package com.example.car_eloung;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class Car_E_Lounge extends AppCompatActivity {
 
@@ -16,6 +19,24 @@ public class Car_E_Lounge extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car__e__lounge);
+
+        Button button = (Button)findViewById(R.id.button);
+        Button button2 = (Button)findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Car_E_Lounge.this, Questionnaire.class));
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Car_E_Lounge.this, Car.class));
+            }
+        });
+
+
 
     }
 
