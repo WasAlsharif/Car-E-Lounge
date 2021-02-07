49b52fb2eae2afab2c01fd6d39529cfc1e808893
WasAlsharif <waseem.m.alsharif@gmail.com>
2021-02-07 22:23:12 +0300

    Adding jpmml dependencies

--- app/src/main/java/com/example/car_eloung/Questionnaire.java
+++ app/src/main/java/com/example/car_eloung/Questionnaire.java
@@ -1,23 +1,13 @@
 package com.example.car_eloung;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import org.jpmml.evaluator.reporting.ReportingValueFactoryFactory;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Questionnaire extends AppCompatActivity {
 
