a53df93b1a413993d1660173b257d871e1357f20
WasAlsharif <waseem.m.alsharif@gmail.com>
2021-03-10 06:18:37 +0300

    Connected the project with firebase and Finished 50% of Compare feature.

--- app/src/main/java/com/example/car_eloung/Rapid_Miner.java
+++ app/src/main/java/com/example/car_eloung/Rapid_Miner.java
@@ -32,13 +32,13 @@ import java.util.List;
 
 
 public class Rapid_Miner extends AppCompatActivity {
-
+    DatabaseReference myRef;
     TextView loading;
-    ImageView Tran,home,start_compare;
+    ImageView Tran, home, start_compare;
     ProgressBar progressBar;
-
     String companys[] = new String[96];
-    int i = 0;
+    int i = 0, j = 0;
+    ArrayList<String> models;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +46,7 @@ public class Rapid_Miner extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         Tran = (ImageView) findViewById(R.id.imageView6);
         loading = (TextView) findViewById(R.id.Loading);
-        start_compare = (ImageView)findViewById(R.id.start_compare);
+        start_compare = (ImageView) findViewById(R.id.start_compare);
         home = (ImageView) findViewById(R.id.home);
 
         home.setOnClickListener(new View.OnClickListener() {
@@ -57,16 +57,17 @@ public class Rapid_Miner extends AppCompatActivity {
             }
         });
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("companys");
+        myRef = database.getReference("companys");
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     String temp = String.valueOf(i);
                     companys[i] = dataSnapshot.child(temp).child("Car Company").getValue().toString();
                     i++;
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
@@ -80,15 +81,52 @@ public class Rapid_Miner extends AppCompatActivity {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
+                models = new ArrayList<>();
                 progressBar.setVisibility(View.INVISIBLE);
                 loading.setVisibility(View.INVISIBLE);
                 Tran.setVisibility(View.INVISIBLE);
                 spinner.setAdapter(spinnerArrayAdapter);
                 spinner2.setAdapter(spinnerArrayAdapter);
+             /*   spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        myRef = database.getReference();
+                        myRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                    String temp = String.valueOf(j);
+                                    if(dataSnapshot.child(temp).child("Car Company").getValue().toString()==spinner.getSelectedItem()){
+                                         models.add(dataSnapshot.child(temp).child("Model").getValue().toString());
+                                    }
+                                     j++;
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                });*/
+
             }
         }, 3000);   //3 seconds
 
-
+/*
+        Spinner model1 = (Spinner) findViewById(R.id.model_1);
+        Spinner model2 = (Spinner) findViewById(R.id.model_2);
+        ArrayAdapter<String> spinnerArrayAdapter3 = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item, models);
+        spinnerArrayAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                model1.setAdapter(spinnerArrayAdapter3);
+            }
+        }, 3000);   //3 seconds
+*/
         start_compare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
