4ee4baefdbf5ae52fe0fb2489dd8513206d88ecd
M-fadel <m.ohm.md2008@hotmail.com>
2021-03-13 22:19:27 +0300

    Merge branch 'master' of C:\Users\hmood\AndroidStudioProjects\Car-E-Lounge with conflicts.

---
+++ app/src/main/java/com/example/car_eloung/combair.java
@@ -0,0 +1,19 @@
+package com.example.car_eloung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class combair extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.compare_activity);
+
+
+        Spinner spinner = (Spinner)findViewById(R.id.car1);
+
+    }
+}
---
+++ app/src/main/java/com/example/car_eloung/combairResult.java
@@ -0,0 +1,14 @@
+package com.example.car_eloung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class combairResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combair_result);
+    }
+}
